cfbc09aa0def2f65e84d79a70755f22199d3b6d4
kheynes <heyneskeziah@gmail.com>
2020-09-23 17:20:11 +0200

    created aircraft class for different aircrafts to inherit from

--- com/avaj_launcher/sim/aircraft/Aircraft.java
+++ com/avaj_launcher/sim/aircraft/Aircraft.java
@@ -1,1 +1,21 @@
 package com.avaj_launcher.sim.aircraft;
+
+import com.avaj_launcher.sim.Coordinates;
+
+
+public class Aircraft {
+    protected long  id;
+    protected String name;
+    protected Coordinates coordinates;
+    private static long idCounter = 0;
+
+    protected Aircraft(String name, Coordinates coordinates) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.id = NextId();
+    }
+
+    private long NextId() {
+        return (idCounter++);
+    }
+}
