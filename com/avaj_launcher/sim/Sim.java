f05f2651d205455c3867f8744ea095c8db135aaf
kheynes <heyneskeziah@gmail.com>
2020-10-03 13:37:02 +0200

    Creating a validate input method

--- com/avaj_launcher/sim/Sim.java
+++ com/avaj_launcher/sim/Sim.java
@@ -7,40 +7,62 @@ import com.avaj_launcher.sim.tower.*;
 public class Sim {
     public static void main(String[] args) {
         BufferedReader fileReader = null;
-        try{
-            if (0 < args.length) {
-                String filename = args[0];
-                File file = new File(filename);
+        // try{
+        //     if (0 < args.length) {
+        //         String filename = args[0];
+        //         File file = new File(filename);
 
-                if (file.exists()) {
-                    if (file.length() != 0){
-                        String currentLine;
-                    } else {
-                        throw new Exception("The file is empty");
-                    }
+        //         if (file.exists()) {
+        //             if (file.length() != 0){
+        //                 String currentLine;
+        //                 int storeNumber;
+
+        //                 fileReader = new BufferedReader(new FileReader(file));
+                        
+        //                 currentLine = fileReader.readLine();
+
+        //                 try { 
+        //                      storeNumber = Integer.parseInt(currentLine);
+        //                     if (storeNumber >= 0) {
+        //                         while((currentLine = fileReader.readLine()) != null) {
+        //                             System.out.println(currentLine);
+        //                         }
+        //                     } else {
+        //                         throw new Exception("The first line was not a positive integer");
+        //                     }
+        //                 } catch (Exception e) {
+        //                     throw new Exception("The first line was not a positive integer");
+        //                 }
+        //             } else {
+        //                 throw new Exception("The file is empty");
+        //             }
+        //         } else {
+        //             throw new Exception("File does not exist");
+        //         }
+        //     }
+        // }catch(Exception e) {
+        //     System.out.println(e.getMessage());
+        // }
+        String line = "Balloon hey hey hey hey";
+        validateInput(line);
+    }
+
+    public static void validateInput(String line) {
+        String[] inputArray = line.split(" ");
+        System.out.println(inputArray[0]);
+
+        try {
+            if(inputArray.length == 5){
+                if(inputArray[0].equals("Helicopter") || inputArray[0].equals("Balloon") || inputArray[0].equals("JetPlane")) {
+                    System.out.println("You is on the right track bish");
                 } else {
-                    throw new Exception("File does not exist");
+                    throw new Exception("First string needs to be aircraft type");
                 }
+            } else {
+                throw new Exception("Incorrect input format");
             }
         }catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
-        
-
-        // AircraftFactory aircraftFactory = new AircraftFactory();
-        // Flyable flyable1 = aircraftFactory.newAircraft("Helicopter", "H1", 1, 1, 1);
-        // Flyable flyable2 = aircraftFactory.newAircraft("Balloon", "B1", 2, 2, 2);
-        // Flyable flyable3 = aircraftFactory.newAircraft("JetPlane", "J1", 3, 3, 3);
-
-        // WeatherTower weatherTower = new WeatherTower();
-        // flyable1.registerTower(weatherTower);
-        // flyable2.registerTower(weatherTower);
-        // flyable3.registerTower(weatherTower);
-
-        // flyable1.updateConditions();
-        // flyable2.updateConditions();
-        // flyable3.updateConditions();
-
     }
 }
