804cdfe30dfde0d39e861f47af01e6f3100900eb
kheynes <heyneskeziah@gmail.com>
2020-10-13 11:02:05 +0200

    weather provider class done.

---
+++ com/avaj_launcher/sim/weather/weatherProvider.java
@@ -0,0 +1,37 @@
+package com.avaj_launcher.sim.weather;
+
+import com.avaj_launcher.sim.Coordinates;
+
+public class WeatherProvider {
+    private WeatherProvider weatherProvider;
+    private String[] weather ={"RAIN", "SUN", "SNOW", "FOG"};
+
+    private WeatherProvider() {
+        weatherProvider = new WeatherProvider();
+    }
+
+    public WeatherProvider getProvider() {
+        return (weatherProvider);
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+        int longitude = coordinates.getLongitude();
+        int latitude = coordinates.getLatitude();
+        int height = coordinates.getHeight();
+        int weatherType = 0;
+
+        longitude = longitude % 301;
+        latitude = latitude % 301;
+
+        if (longitude <= 150 && latitude <= 150) { 
+            weatherType = 0;
+        } else if (longitude <= 150 && latitude > 150) {
+            weatherType = 1;
+        } else if (longitude > 150 && latitude <= 150) {
+            weatherType = 2;
+        } else if (longitude > 150 && latitude > 150) {
+            weatherType = 3;
+        }
+        return (weather[weatherType]);
+    }
+}
